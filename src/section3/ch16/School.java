3c00c5e5158a2f65d6f9137d5a021571aa91bdca
talk105 <talk105@naver.com>
2023-08-13 12:37:34 +0900

    3-16

---
+++ src/section3/ch16/School.java
@@ -0,0 +1,14 @@
+package section3.ch16;
+
+public class School {
+    private static School instance = new School();
+
+    private School() {}
+
+    public static School getInstance() {
+        if(instance == null) {
+            instance = new School();
+        }
+        return instance;
+    }
+}
