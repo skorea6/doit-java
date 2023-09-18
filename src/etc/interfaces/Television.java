c8381f622c8b3316ca595d34675204d7fede1768
talk105 <talk105@naver.com>
2023-09-18 22:47:48 +0900

    interfaces final

---
+++ src/etc/interfaces/Television.java
@@ -0,0 +1,27 @@
+package etc.interfaces;
+
+public class Television implements RemoteControl {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("티비를 켭니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("티비를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        if(volume > RemoteControl.MAX_VOLUME){
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if (volume < RemoteControl.MIN_VOLUME) {
+            this.volume = RemoteControl.MIN_VOLUME;
+        }else{
+            this.volume = volume;
+        }
+        System.out.println("현재 티비 볼륨 : " + this.volume);
+    }
+}
