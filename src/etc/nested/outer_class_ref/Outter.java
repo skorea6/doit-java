d9d65fe881aad0c8de715a63ab6efb4401497413
talk105 <talk105@naver.com>
2023-09-29 00:23:51 +0900

    Outer

---
+++ src/etc/nested/outer_class_ref/Outter.java
@@ -0,0 +1,25 @@
+package etc.nested.outer_class_ref;
+
+public class Outter {
+    String field = "Outter-field";
+    void method() {
+        System.out.println("Outter-method");
+    }
+
+    class Nested{
+        String field = "Nested-field";
+        void method() {
+            System.out.println("Nested-method");
+        }
+
+        void print() {
+            // Nested 쪽
+            System.out.println(this.field);
+            this.method();
+
+            // Outter 쪽
+            System.out.println(Outter.this.field);
+            Outter.this.method();
+        }
+    }
+}
